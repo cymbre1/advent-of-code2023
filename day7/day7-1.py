9bb96a481ece8b54a2e82d622bcfd8477620c00e
Cymbre Spoehr <cymbre.spoehr@gentex.com>
2023-12-08 15:02:50 -0500

    Not quite working :(

--- day7/day7-1.py
+++ day7/day7-1.py
@@ -34,29 +34,23 @@ def is_high_card(card: str):
 input_file = open("day7/input.txt", "r")
 input_lines = input_file.readlines()
 
-high_cards = list(line for line in input_lines if is_high_card(line.split(" ")[0]))
+all_hands = []
 
-for card in high_cards:
+all_hands += sorted(list(line for line in input_lines if is_high_card(line.split(" ")[0])), reverse=True, key=lambda x: [possible_cards.index(c) for c in x.split(" ")[0]])
+all_hands += sorted(list(line for line in input_lines if is_one_pair(line.split(" ")[0])), reverse=True, key=lambda x: [possible_cards.index(c) for c in x.split(" ")[0]])
+all_hands += sorted(list(line for line in input_lines if is_two_pair(line.split(" ")[0])), reverse=True, key=lambda x: [possible_cards.index(c) for c in x.split(" ")[0]])
+all_hands += sorted(list(line for line in input_lines if is_three_of_a_kind(line.split(" ")[0])), reverse=True, key=lambda x: [possible_cards.index(c) for c in x.split(" ")[0]])
+all_hands += sorted(list(line for line in input_lines if is_full_house(line.split(" ")[0])), reverse=True, key=lambda x: [possible_cards.index(c) for c in x.split(" ")[0]])
+all_hands += sorted(list(line for line in input_lines if is_four_of_a_kind(line.split(" ")[0])), reverse=True, key=lambda x: [possible_cards.index(c) for c in x.split(" ")[0]])
+all_hands += sorted(list(line for line in input_lines if is_five_of_a_kind(line.split(" ")[0])), reverse=True, key=lambda x: [possible_cards.index(c) for c in x.split(" ")[0]])
+
+total = 0
+for card in all_hands:
     print(card)
-    for letter in card.split(" ")[0]:
-        # print(letter)
-        print(possible_cards.index(letter))
-# print(high_cards.sort(key=lambda x: possible_cards.index(x[0])))
+    print(int(card.split(" ")[1].replace("\n", "")))
+    # print((all_hands.index(card) + 1))
+    total += int(card.split(" ")[1].replace("\n", "")) * (all_hands.index(card) + 1)
+print(total)
 
-# one_pairs = list(line for line in input_lines if is_one_pair(line.split(" ")[0]))
-# print(one_pairs.sort(key=lambda element: possible_cards.index(element)))
-
-# two_pairs = list(line for line in input_lines if is_two_pair(line.split(" ")[0]))
-# print(two_pairs.sort(key=lambda element: possible_cards.index(element)))
-
-# three_of_a_kind = list(line for line in input_lines if is_three_of_a_kind(line.split(" ")[0]))
-# print(three_of_a_kind.sort(key=lambda element: possible_cards.index(element)))
-
-# full_houses = list(line for line in input_lines if is_full_house(line.split(" ")[0]))
-# print(full_houses.sort(key=lambda element: possible_cards.index(element)))
-
-# four_of_a_kind = list(line for line in input_lines if is_four_of_a_kind(line.split(" ")[0]))
-# print(four_of_a_kind.sort(key=lambda element: possible_cards.index(element)))
-
-# five_of_a_kind = list(line for line in input_lines if is_five_of_a_kind(line.split(" ")[0]))
-# print(five_of_a_kind.sort(key=lambda element: possible_cards.index(element)))
+all_hand_scores = sum(int(card.split(" ")[1].replace("\n", "")) * (all_hands.index(card) + 1) for card in all_hands)
+print(all_hand_scores)
